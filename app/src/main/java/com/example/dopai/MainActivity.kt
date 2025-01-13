5ddfe24ced07a25701286a73725ad2f7e5a06f70
Devashis <devagope24@gmail.com>
2025-01-13 20:00:09 +0530

    first commit

---
+++ app/src/main/java/com/example/dopai/MainActivity.kt
@@ -0,0 +1,54 @@
+
+package com.example.dopai
+
+import android.graphics.Typeface
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.ai.client.generativeai.BuildConfig
+import com.google.ai.client.generativeai.GenerativeModel
+import kotlinx.coroutines.runBlocking
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val vprompt = findViewById<EditText>(R.id.etprompt)
+        val btnsbt = findViewById<Button>(R.id.btnsubmt)
+        val reslt  = findViewById<TextView>(R.id.tvresult)
+
+        btnsbt.setOnClickListener {
+            val prompt = vprompt.text.toString()
+
+            val generativeModel = GenerativeModel(
+                modelName = "gemini-pro",
+                apiKey = "AIzaSyDiez4u-3NUMskLUNtjLVWAqZDb5cSp_Eo"
+            )
+            runBlocking {
+                val response = generativeModel.generateContent(prompt)
+                reslt.text = response.text
+            }
+
+
+        }
+
+    }
+
+}
+
+
